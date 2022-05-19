9584a5ba7e6e7f75c508191d2e2d90fe3e98bb98
MadanStha7 <madanstha.124@gmail.com>
2022-05-19 11:34:39 +0545

    Control FLows in Dart

---
+++ Control Flow/if-statement.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  var is_login = true;
+  if (is_login) {
+    print("Welcome Madan");
+  }
+
+  var a = 10;
+  if (a == 10) {
+    print('Value of a is $a');
+  }
+}
